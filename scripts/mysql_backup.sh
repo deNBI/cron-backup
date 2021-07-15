559e35c3f962555538eadc31c99d0a0a3a769e27
vktrrdk <vrudko@techfak.uni-bielefeld.de>
2021-07-15 15:04:32 +0200

    finally

--- scripts/mysql_backup.sh
+++ scripts/mysql_backup.sh
@@ -3,4 +3,5 @@
 NOW=$(date '+%y-%m-%d-%H%M')
 
 FILE=/etc/backup/db-${NOW}.sql.gz
-mysqldump -h limesurvey_db -u root -p ${LIMESURVEY_DB_PASSWORD} --all-databases | gzip > $FILE
+mysqldump -h limesurvey_db -u root --password=${LIMESURVEY_DB_PASSWORD} --all-databases | gzip > $FILE
+

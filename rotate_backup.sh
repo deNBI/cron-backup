cc98a2b727382a2b9e90fa5060667ed416be44d1
dweinholz <david-weinholz@web.de>
2021-08-20 13:30:30 +0200

    updated

--- rotate_backup.sh
+++ rotate_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SIZE_TYPE="GiB"
-SIZE_TYPE_OPTIONS=(KB KiB MB MiB GB GiB TB TiB)
+SIZE_TYPE_OPTIONS=(MB MiB GB GiB TB TiB)
 MAX_SIZE=2
 CUT_SIZE=1
 while getopts d:m:c:t: flag; do
@@ -16,7 +16,7 @@ done
 
 type_exists=$(echo "${SIZE_TYPE_OPTIONS[@]}" | grep -o $SIZE_TYPE | wc -w)
 
-if [ "$type_exists" -eq "2" ]; then
+if [ "$type_exists" -ne "1" ]; then
   echo "Size Type [$SIZE_TYPE] Not Valid - Valid Types: ${SIZE_TYPE_OPTIONS[*]}"
   exit
 fi
@@ -49,9 +49,15 @@ echo "Cutsize: $CUT_SIZE $SIZE_TYPE ($CUT_SIZE_BYTES Bytes)"
 
 # Check Size of BACKUP_DIR
 CURRENT_SIZE_BYTES=$(du -bs "$DIRECTORY" | cut -f1)
-echo "Current Size $DIRECTORY: $CURRENT_SIZE_BYTES Bytes"
+
 if [ $CURRENT_SIZE_BYTES -gt $MAX_SIZE_BYTES ]; then
-  echo "Maximum Size $MAX_SIZE exceeded"
-else
-  echo "Maximum size not yet reached"
+
+  while [ $CURRENT_SIZE_BYTES -gt $CUT_SIZE_BYTES ]; do
+    echo "Maximum Size $MAX_SIZE exceeded - Current Size  $CURRENT_SIZE_BYTES"
+    CURRENT_SIZE_BYTES=$(du -bs "$DIRECTORY" | cut -f1)
+
+    rm "$(ls -t | tail -1)"
+
+  done
 fi
+echo "Maximum size not yet reached"

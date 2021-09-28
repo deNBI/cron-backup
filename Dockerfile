994e9b3875d2e55c08b756a7bcff20c2a52fa468
dweinholz <david-weinholz@web.de>
2021-09-28 16:16:14 +0200

    refactored config usage

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,5 @@ COPY ./backup/rotate_backup.sh /rotate_backup.sh
 COPY ./backup/backup-cron /backup-cron
 COPY ./s3/s3_backup.sh /s3_backup.sh
 COPY ./s3/s3_backup-cron /s3_backup-cron
-COPY ./s3/s3cmd.cfg /root/.s3cfg
 RUN chmod +x /prepare-cron.sh
 ENTRYPOINT ["/prepare-cron.sh"]
--- s3/s3_backup.sh
+++ s3/s3_backup.sh
@@ -4,12 +4,10 @@
 basedir="/etc/backup"
 s3path=$S3_PATH
 
-find  $S3_CONFIGS_PATH  -type f  -name "*.cfg"| while read -r a; do
-  echo "$a"
+find  $S3_CONFIGS_PATH  -type f  -name "*.cfg"| while read -r cfg; do
+  echo "$cfg"
 
-  . $a
-  echo $S3_ACCESS_KEY
-  done
+
   s3cmd mb s3://$S3_PATH
   cd $basedir
 
@@ -25,8 +23,8 @@ find  $S3_CONFIGS_PATH  -type f  -name "*.cfg"| while read -r a; do
     source "$S3_HASHDIR/$fnamehash" 2>/dev/null
 
     if [ "$filehash" != "$storedhash" ]; then
-      s3cmd put -e $a s3://$S3_PATH/$a
-      if [ $? -eq 0]; then
+      s3cmd put -c "$cfg" -e $a s3://$S3_PATH/$a
+      if [ $? -eq 0 ]; then
       echo "storedhash='$filehash'" >"$S3_HASHDIR/$fnamehash"
       fi
     else

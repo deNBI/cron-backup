83eabdbcf80d1f599fcdf7be26d57f17dd2d1fd3
dweinholz <david-weinholz@web.de>
2022-03-14 22:00:54 +0100

    Update s3_backup_rotation.sh

--- s3/s3_backup_rotation.sh
+++ s3/s3_backup_rotation.sh
@@ -9,6 +9,7 @@ find $S3_CONFIGS_PATH -type f -name "*.cfg" | while read -r env_data; do
   config_name=$(basename -- $env_data)
   site_name="${config_name%.*}"
   s3path=$s3path/$site_name/
+  baseEncryptDir="/etc/encrypted/backup/"$site_name
   echo $s3path
   . "$env_data"
   echo "Delete Files uploaded more than $S3_BACKUP_ROTATION_TIME_LIMIT days ago..."
@@ -30,8 +31,13 @@ find $S3_CONFIGS_PATH -type f -name "*.cfg" | while read -r env_data; do
 
     if [[ $createDate -le $olderThan ]]; then
       if [ $fileName != "" ]; then
-        printf '    Deleting "%s"\n' $fileName
+        printf 'Deleting "%s" in S3 \n' $fileName
         s3cmd -c "$tmp_conf" del "$fileName"
+        printf 'Also Deleting local encrypted/unencrypted file if still exists'
+        rm -f $baseEncryptDir/$fileName
+        unencryptedFile=/etc/unencrypted/$fileName
+        printf 'Delete local unencrypted file "%s" if it still exists\n' $unencryptedFile
+        rm -f ${unencryptedFile%.gpg}
       fi
 
     else

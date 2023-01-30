6df6d45f605eb56b61d02a7932eba54432deba86
dweinholz <david-weinholz@web.de>
2023-01-30 11:55:25 +0100

    fixed non interavtive

--- postgresql/install-packages.sh
+++ postgresql/install-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-apt-get   update && apt-get install -y postgresql
+export DEBIAN_FRONTEND=noninteractive
+apt-get   update && apt-get install -y postgresql-client-common
 
 echo "Installed dependencies"

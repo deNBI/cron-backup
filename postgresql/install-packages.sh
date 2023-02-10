82a0f18167533a3df17eb49454baae8ff76a56a7
dweinholz <david-weinholz@web.de>
2023-02-10 10:21:55 +0100

    added last postgres envs

--- postgresql/install-packages.sh
+++ postgresql/install-packages.sh
@@ -5,6 +5,6 @@ apt-get   update && apt-get install -y postgresql-client
 
 echo "Installed dependencies"
 
-declare -p | grep -E 'POSTGRES_HOST|POSTGRES_USER|POSTGRES_DB' >>/container.env
+declare -p | grep -E 'POSTGRES_HOST|POSTGRES_USER|POSTGRES_DB|POSTGRES_PORT|POSTGRES_PASSWORD' >>/container.env
 
 echo "Environment was set"

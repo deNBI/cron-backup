05bd0ed766d578a8f6579f807a11dc918a55558d
dweinholz <d.weinholz@fz-juelich.de>
2026-04-07 06:01:26 +0000

    updtedlogging

--- backup/notify_uptime_kuma.sh
+++ backup/notify_uptime_kuma.sh
@@ -3,16 +3,20 @@
 # Set KUMA_STATUS_ENDPOINT variable from environment or default to empty string
 KUMA_STATUS_ENDPOINT=${KUMA_STATUS_ENDPOINT:-}
 
+log() {
+  echo "[$(date +"%Y-%m-%d %H:%M:%S")] - $1"
+}
+
 if [ -z "$KUMA_STATUS_ENDPOINT" ]; then
-    echo "INFO: KUMA_STATUS_ENDPOINT is not set. Skipping."
+    log "INFO: KUMA_STATUS_ENDPOINT is not set. Skipping."
 else
     # Use curl to make a GET request to the status endpoint
     response=$(curl -s -X GET "$KUMA_STATUS_ENDPOINT")
 
     # Check if the request was successful
     if [ $? -eq 0 ]; then
-        echo "Status endpoint responded successfully: $response"
+        log "Status endpoint responded successfully: $response"
     else
-        echo "Error: Failed to push status from $KUMA_STATUS_ENDPOINT. Status code: $?"
+        log "Error: Failed to push status from $KUMA_STATUS_ENDPOINT. Status code: $?"
     fi
 fi
--- s3/s3_notify_uptime_kuma.sh
+++ s3/s3_notify_uptime_kuma.sh
@@ -2,17 +2,19 @@
 
 # Set KUMA_STATUS_ENDPOINT variable from environment or default to empty string
 S3_KUMA_STATUS_ENDPOINT=${S3_KUMA_STATUS_ENDPOINT:-}
-
+log() {
+  echo "[$(date +"%Y-%m-%d %H:%M:%S")] - $1"
+}
 if [ -z "$S3_KUMA_STATUS_ENDPOINT" ]; then
-    echo "INFO: S3_KUMA_STATUS_ENDPOINTT is not set. Skipping."
+    log "INFO: S3_KUMA_STATUS_ENDPOINTT is not set. Skipping."
 else
     # Use curl to make a GET request to the status endpoint
     response=$(curl -s -X GET "$S3_KUMA_STATUS_ENDPOINT")
 
     # Check if the request was successful
     if [ $? -eq 0 ]; then
-        echo "Status endpoint responded successfully: $response"
+        log "S3 Status endpoint responded successfully: $response"
     else
-        echo "Error: Failed to push status from $S3_KUMA_STATUS_ENDPOINT. Status code: $?"
+        log "Error: Failed to push status from S3-Status $S3_KUMA_STATUS_ENDPOINT. Status code: $?"
     fi
 fi
